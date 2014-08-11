608a3891f59b4a3a1bc7eae1a10c8cd67a18419f
stoeffel <schtoeffel@gmail.com>
2014-08-11 14:59:00 +0200

    updates

---
+++ apts.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+apt-get install chromium-browser git nodejs-legacy zsh i3 i3status dmenu openssh-server apache2 supervisor php5 php5-cli libapache2-mod-php5 php5-gd php5-json php5-ldap php5-mysql php5-pgsql npm pgadmin3 postgresql postgresql postgresql-contrib
