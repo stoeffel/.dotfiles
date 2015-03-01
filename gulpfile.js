9a8f248e5bf2e271cbcf62c7a9fa4625d9ab2a91
stoeffel <schtoeffel@gmail.com>
2015-03-01 21:02:44 +0100

    move symlinks to link task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 var gulp = require('gulp');
+var symlink = require('gulp-symlink');
 var R = require('ramda');
 
 
 var symlinks = require('./symlinks.json');
 
 gulp.task('link', function() {
-  R.forEach(function(item) {
-    var destination = R.head(item);
-    var link = R.tail(item)[0];
-    console.log('ln -sf ', destination, link);
-  }, symlinks);
+  var dests = R.map(R.head, symlinks);
+  var links = R.flatten(R.map(R.tail, symlinks));
+
+  return gulp.src(dests)
+    .pipe(symlink(links, { force: true }));
 });
